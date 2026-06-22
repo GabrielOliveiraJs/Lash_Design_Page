a991f6052298f820d47aaf115029884066e659dc
Gabriel de Oliveira Pinto <99537756+GabrielOliveiraJs@users.noreply.github.com>
2026-06-22 16:56:39 -0300

    barra-de-navegacao-pronta

---
+++ script.js
@@ -0,0 +1,41 @@
+// Alternar o underline do link clicado na navbar
+let navbarLinks = document.querySelectorAll(".navigation-link");
+
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarLinks.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    link.classList.add("active");
+  });
+});
+
+// Menu de hambúrguer para dispositivos móveis
+const menuToggle = document.querySelector(".menu-toggle");
+const navList = document.querySelector(".navigation-list");
+const navLinks = document.querySelectorAll(".navigation-link");
+
+function abrirMenu() {
+  navList.classList.add("active");
+  menuToggle.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+}
+
+function fecharMenu() {
+  navList.classList.remove("active");
+  menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+}
+
+menuToggle.addEventListener("click", () => {
+  if (navList.classList.contains("active")) {
+    fecharMenu();
+  } else {
+    abrirMenu();
+  }
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    fecharMenu();
+  });
+});
