f017a584921258c3e570088803cc41e0c3c62f3e
Gabriel de Oliveira Pinto <99537756+GabrielOliveiraJs@users.noreply.github.com>
2026-06-29 17:35:43 -0300

    adicao-do-js-dos-cards

--- js/script.js
+++ js/script.js
@@ -39,3 +39,43 @@ navLinks.forEach((link) => {
     fecharMenu();
   });
 });
+
+/* Inclusão dos cards na página products */
+
+const cardsContainer = document.querySelector(".cards-container");
+const cardTemplate = document.querySelector("#card-template");
+
+async function carregarDados() {
+  try {
+    const response = await fetch("../data/products.json");
+
+    if (!response.ok) {
+      throw new Error("Erro ao carregar products.json");
+    }
+
+    return await response.json();
+  } catch (erro) {
+    console.error(erro);
+    return [];
+  }
+}
+
+async function iniciar() {
+  linksList = await carregarDados();
+
+  linksList.forEach((product) => {
+    const card = cardTemplate.content.cloneNode(true);
+
+    // card.classList.remove("card-model");
+    card.querySelector(".card-image").src = product.imagem;
+    card.querySelector(".card-image").alt = product.nome;
+    card.querySelector(".card-title").textContent = product.nome;
+    card.querySelector(".card-description").textContent = product.descricao;
+    card.querySelector(".card-tag").textContent = product.recomendacao
+    card.querySelector(".card-link").href = product.link;
+
+    cardsContainer.appendChild(card);
+  });
+}
+
+iniciar();
