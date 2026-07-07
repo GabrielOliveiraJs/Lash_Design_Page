7bfa056e487af150483b83d2a508eb883dcca6b0
Gabriel de Oliveira Pinto <99537756+GabrielOliveiraJs@users.noreply.github.com>
2026-07-07 11:12:32 -0300

    criacao-secoes-hero-e-about

---
+++ js/footer.js
@@ -0,0 +1,4 @@
+const copyrightText = document.querySelector('.copyright-text')
+const year = new Date().getFullYear()
+
+copyrightText.textContent += ` ${year} May Souza Beauty. Todos os direitos reservados.`
