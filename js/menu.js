3d8273bec9f2609cff8016a778804c38c5d08c4c
Gabriel de Oliveira Pinto <99537756+GabrielOliveiraJs@users.noreply.github.com>
2026-07-10 16:50:13 -0300

    adicao-sticky-navbar

--- js/menu.js
+++ js/menu.js
@@ -40,3 +40,13 @@ navLinks.forEach((link) => {
   });
 });
 
+// Efeito Sticky
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
