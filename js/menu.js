25acd5966ab1710650d1e24f4bc43cd6a185bf53
Gabriel de Oliveira Pinto <99537756+GabrielOliveiraJs@users.noreply.github.com>
2026-07-20 12:32:01 -0300

    preparacao-da-navbar-para-pagina-de-cilios

--- js/menu.js
+++ js/menu.js
@@ -1,48 +1,55 @@
+// Variáveis
+const menuToggle = document.querySelector(".menu-toggle");
+const menuIcon = document.querySelector(".menu-icon");
+const navList = document.querySelector(".navigation-list");
+const navLinks = document.querySelectorAll(".navigation-link");
+const currentPage = window.location.pathname.split("/").pop() || "index.html";
+const currentHash = window.location.hash;
+const header = document.querySelector("header");
+
 // Alternar o underline do link clicado na navbar
-let navbarLinks = document.querySelectorAll(".navigation-link");
-
-navbarLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    navbarLinks.forEach((item) => {
-      item.classList.remove("is-active");
-    });
-
-    link.classList.add("is-active");
-  });
+navLinks.forEach((link) => {
+  const href = link.getAttribute("href");
+  if (href.includes("#")) {
+    if (href.endsWith(currentHash) && currentHash !== "") {
+      link.classList.add("active");
+    }
+    return;
+  }
+  const pageName = href.split("/").pop();
+  if (pageName === currentPage && currentHash === "") {
+    link.classList.add("active");
+  }
 });
 
 // Menu de hambúrguer para dispositivos móveis
-const menuToggle = document.querySelector(".menu-toggle");
-const navList = document.querySelector(".navigation-list");
-const navLinks = document.querySelectorAll(".navigation-link");
-
-function abrirMenu() {
+function openMenu() {
   navList.classList.add("is-active");
-  menuToggle.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  menuToggle.setAttribute("aria-expanded", "true");
+  menuIcon.classList.remove("fa-bars");
+  menuIcon.classList.add("fa-xmark");
 }
 
-function fecharMenu() {
+function closeMenu() {
   navList.classList.remove("is-active");
-  menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
+  menuToggle.setAttribute("aria-expanded", "false");
+  menuIcon.classList.remove("fa-xmark");
+  menuIcon.classList.add("fa-bars");
 }
 
 menuToggle.addEventListener("click", () => {
   if (navList.classList.contains("is-active")) {
-    fecharMenu();
+    closeMenu();
   } else {
-    abrirMenu();
+    openMenu();
   }
 });
 
 navLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    fecharMenu();
-  });
+  link.addEventListener("click", closeMenu);
 });
 
 // Efeito Sticky
-const header = document.querySelector("header");
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 20) {
     header.classList.add("scrolled");
